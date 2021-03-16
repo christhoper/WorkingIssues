1877946566ca0947e2dead3e5b651c8789c14d43
hendy <309875551@qq.com>
2021-03-16 18:01:28 +0800

    继续网络方面之旅

--- WorkingIssues/DesignDemol/行为型Demol/Mediator.swift
+++ WorkingIssues/DesignDemol/行为型Demol/Mediator.swift
@@ -6,3 +6,70 @@
 //
 
 import Foundation
+//MARK: - 中介者
+/*
+ 用一个中介者对象封装的对象交互，中介者使各对象不需要显示的相互作用，从而使耦合松散，而且可以独立的改变它们之间的交互
+ */
+
+/// 接受者
+protocol Receiver {
+    // 关联类型
+    associatedtype MessageType
+    func receive(message: MessageType)
+}
+
+protocol Sender {
+    
+    associatedtype MessageType
+    associatedtype ReceiverType: Receiver
+    
+    var recipients: [ReceiverType] { get }
+    func send(message: MessageType)
+}
+
+struct Programmer: Receiver {
+    
+    let name: String
+    init(name: String) {
+        self.name = name
+    }
+    
+    func receive(message: String) {
+        print("\(name)-receive-\(message)")
+    }
+}
+
+//MARK: - 中介者对象
+final class MessageMediator: Sender {
+    
+    var recipients: [Programmer] = []
+    
+    func add(recipient: Programmer) {
+        recipients.append(recipient)
+    }
+    
+    func send(message: String) {
+        recipients.forEach { (programmer) in
+            programmer.receive(message: message)
+        }
+    }
+}
+
+
+class MediatorTest {
+    
+    init() {}
+    
+    func test() {
+        let messageMediator = MessageMediator()
+        let programmer1 = Programmer(name: "Hendy")
+        let programmer2 = Programmer(name: "Chris")
+        messageMediator.add(recipient: programmer1)
+        messageMediator.add(recipient: programmer2)
+        spamMoster(message: "想做什么事情", worker: messageMediator)
+    }
+    
+    private func spamMoster(message: String, worker: MessageMediator) {
+        worker.send(message: message)
+    }
+}
--- WorkingIssues/DesignDemol/行为型Demol/Memento.swift
+++ WorkingIssues/DesignDemol/行为型Demol/Memento.swift
@@ -6,3 +6,46 @@
 //
 
 import Foundation
+
+//MARK: - 备忘录
+/*
+ 在不破坏封装性的前提下，捕获一个对象的内部状态，并在该对象之外保存这个状态。这样就可以将该对象恢复到原先保存的状态
+ */
+
+/// 备忘录
+typealias Memento = [String: String]
+
+
+//MARK: - 发起人（Originator）
+protocol MementoConvertible {
+    var memento: Memento { get }
+    init?(memento: Memento)
+}
+
+struct GameState: MementoConvertible {
+    
+    private enum Keys {
+        static let chapter = "com.1"
+        static let weapon = "com.2"
+    }
+    
+    var chapter: String
+    var weapon: String
+    
+    init(chapter: String, weapon: String) {
+        self.chapter = chapter
+        self.weapon = weapon
+    }
+    
+    init?(memento: Memento) {
+        guard let mementoChapter = memento[Keys.chapter],
+              let mementoWeapon = memento[Keys.weapon] else { return nil }
+        chapter = mementoChapter
+        weapon = mementoWeapon
+        
+    }
+    
+    var memento: Memento {
+        return [Keys.chapter: chapter, Keys.weapon: weapon]
+    }
+}
